5ebc0bd9e10851af9cc8d47f59d64bec7bf0e22b
JuanWilcke <46919034@est.ort.edu.ar>
2022-04-19 11:44:59 -0300

    ex alt

--- Assets/Consigna.cs
+++ Assets/Consigna.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Consigna : MonoBehaviour
 {
+    public int edad;
+    public float altura;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (edad<13 || altura<1.2)
+        {
+            Debug.Log("Acceso denegado");
+        }
+        else if (edad >= 13 || altura >= 1.2)
+        {
+            Debug.Log("Acceso permitido");
+        }
+        else if (edad < 1 || altura < 0)
+        {
+            Debug.Log("Error");
+        }
     }
 
     // Update is called once per frame
